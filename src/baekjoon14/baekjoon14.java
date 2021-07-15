79c68876a3b13bca27bd8318e0b83c64339216d1
jhon-rjf <ykjung0917@kakao.com>
2021-07-15 18:00:17 +0900

    0715 study

---
+++ src/baekjoon14/baekjoon14.java
@@ -0,0 +1,24 @@
+package baekjoon14;
+
+import java.util.Scanner;
+
+public class baekjoon14 {
+    public static void main(String []args) {
+    Scanner sc = new Scanner(System.in);
+    int A = sc.nextInt();
+    sc.close();
+    if (A%4 == 0){
+        if(A%100 == 0 && A%400 != 0){
+        System.out.println("0");
+
+        } else if (A%100 == 0 && A%400 == 0){
+            System.out.println("1");
+        }
+    System.out.println("1");
+    }else{
+        System.out.println("0");
+    }
+
+
+    }
+}
