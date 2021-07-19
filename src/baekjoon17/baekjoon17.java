a416805e4ae3284aace32363b2357bda8f902969
jhon-rjf <ykjung0917@kakao.com>
2021-07-19 17:53:05 +0900

    0719 study

---
+++ src/baekjoon17/baekjoon17.java
@@ -0,0 +1,20 @@
+package baekjoon17;
+
+import java.util.Scanner;
+
+public class baekjoon17 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        sc.close();
+        System.out.println(A+" * 1 = "+A*1);
+        System.out.println(A+" * 2 = "+A*2);
+        System.out.println(A+" * 3 = "+A*3);
+        System.out.println(A+" * 4 = "+A*4);
+        System.out.println(A+" * 5 = "+A*5);
+        System.out.println(A+" * 6 = "+A*6);
+        System.out.println(A+" * 7 = "+A*7);
+        System.out.println(A+" * 8 = "+A*8);
+        System.out.println(A+" * 9 = "+A*9);
+    }
+}
