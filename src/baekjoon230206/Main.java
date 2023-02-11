f71b3a94d07cc699aa13c2cde737d1c8e580acd1
YunGeol Jung <61873563+jhon-rjf@users.noreply.github.com>
2023-02-11 23:29:23 +0900

    (NoSuchElement) runtimeerror

--- src/baekjoon230206/Main.java
+++ src/baekjoon230206/Main.java
@@ -12,21 +12,24 @@ public class Main{
         int summ = 0;
         int a = 1;
         String OO = new String("O");
+      
+      
         for (int i = 0; i<Count; i++){
             String ox = sc.next();
             int strlength = ox.length();
         String O [] = new String[strlength];
                 O = ox.split("");
              System.out.println(Arrays.toString(O)); //array에 하나씩 등록가능하게 함. arr[1] = O arr[2] =X 처럼.
+             
+             
              for (int z = 0; z<strlength; z++){
                    boolean tr = (O[z].equals(OO)); //true
-                   
                    System.out.println(z+1+"     1"+tr);
                    sum = sum+a;
                     System.out.println("   2 sum1 "+sum);
                     if ( tr ) { //OOXOXOOOXX (O[i].equals(new String"O")) = ture
                        if(z+1<strlength){
-                         System.out.println("   3 z+1통과"); //exception 예외 
+                         System.outjhon-rjf/baekjoon_study/src/baekjoon230210.println("   3 z+1통과"); //exception 예외 
                         boolean tr1 = (O[z].equals(O[z+1])&&O[z].equals(OO)); //1번 2번 같아?
                          if (tr1){ // OOOXXOO
                 System.out.println("   4 ==통과"); //에러: 더하기할때 2번3번이 같은경우 중복해서 들어간다는것, 하나짜리는 넘어가게끔 되어있는 로직 
--- src/baekjoon230210/Main.java
+++ src/baekjoon230210/Main.java
@@ -2,7 +2,70 @@ import java.util.Scanner;
 import java.util.Arrays;
 public class Main{
 
-    public static void Main(String[] args){
-       
+public Main(int Count)
+    
+    {
+    this.Count = Count; //this 는 이 Main클래스 속의 a에 넣겠다는것.
+    }
+
+String [] func()
+    {
+        
+        
+           Scanner sc = new Scanner(System.in);
+           // System.out.println("OX값");
+            String ox = sc.next();
+            this.strlength = ox.length();
+           this.O = new String[strlength];
+            this.O = ox.split("");
+            
+        
+    
+        return this.O;
+    }
+
+int calc()
+    { 
+    String OO = new String("O");
+    this.sum=0;
+    for (int z = 0; z<strlength; z++)
+        {
+        boolean tr = (O[z].equals(OO)); //O[z]가 O일때
+        if (tr) 
+            {
+            this.temp += 1;
+            this.sum += this.temp;
+            } 
+        else 
+            {
+                this.temp =0;
+            }
+            this.total = this.sum;
+        }
+    return this.total;
+    }
+
+
+    int strlength;
+    int Count;
+    String [] O ;
+    int total;
+    int sum;
+    int temp;
+
+    public static void main(String[] args){
+      Scanner sc = new Scanner(System.in);  
+      //System.out.println("몇번이나?");
+      int Counta = sc.nextInt();
+      Main obj = new Main (Counta);
+      
+      for(int i =0; i<Counta;i++){
+        String prt = Arrays.toString(obj.func());
+     //   System.out.println(prt);
+      System.out.println(obj.calc());
+      
+      }
+      
+
     }
 }
