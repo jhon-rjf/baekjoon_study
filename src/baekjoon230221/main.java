6897af70153565c3dfa490fef680c7d9fe7c6e86
YunGeol Jung <61873563+jhon-rjf@users.noreply.github.com>
2023-02-23 22:12:48 +0900

    Pc 꺼지려하네

--- src/baekjoon230221/main.java
+++ src/baekjoon230221/main.java
@@ -1,22 +1,20 @@
 import java.util.Scanner;
 import java.util.Arrays;
+import java.lang.math;
 public class Main {
 	public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt(); //자리수 
-        String in = sc.next();
-        String total ="";
-        for (int i = 0; i < N; i++){
-            int strlength = in.length();
-            String array [] = new String[strlength];
-            array = in.split("");
-            for(int z = 0; z<strlength; z++){
-        total = total+array[z];
+        int in = sc.nextInt();
+        int total = 0;
+        for (int i = 0; i<N; i++){
+            int temp = 0;
+            double pow = Math.pow
+        } 
         System.out.println(total);
-            }      
+              sc.close();}      
       
 
 	
-  sc.close();}
 }
-}
+
