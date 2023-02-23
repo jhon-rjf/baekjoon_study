7a6a0fe1ca7ed0ecfc0120eca7c17ddb90bbda44
YunGeol Jung <61873563+jhon-rjf@users.noreply.github.com>
2023-02-23 23:03:11 +0900

    230223end

--- src/baekjoon230221/main.java
+++ src/baekjoon230221/main.java
@@ -1,16 +1,24 @@
 import java.util.Scanner;
 import java.util.Arrays;
-import java.lang.math;
 public class Main {
 	public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt(); //자리수 
-        int in = sc.nextInt();
-        int total = 0;
-        for (int i = 0; i<N; i++){
-            int temp = 0;
-            double pow = Math.pow
-        } 
+       int total = 0;
+       int x = sc.nextInt();
+       String n = sc.next();
+        String array [] = new String[x];
+        array = n.split("");
+        for (String s : array){
+            total += Integer.parseInt(s);
+        }
+
+
+
+
+
+
+
+
         System.out.println(total);
               sc.close();}      
       
