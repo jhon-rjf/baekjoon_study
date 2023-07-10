f0bc41a4d5f8e02332b119c4df3ceae23d1f5976
YunGeol Jung <61873563+jhon-rjf@users.noreply.github.com>
2023-07-10 22:34:19 +0900

    108page 3.

--- src/20230710/Main.java
+++ src/20230710/Main.java
@@ -1,31 +1,16 @@
-import java.util.Scanner;
+
 public class Main{
- //2번
+ //3번
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-       System.out.print("정수를 입력하세요:");
-        int input= sc.nextInt();
-     if(input == 1){
-        System.out.println("ONE");
-     }else if(input == 2){
-        System.out.println("TWO");
-     }else if(input == 3){
-        System.out.println("THREE");
-     }else if(input == 4){
-        System.out.println("FOUR");
-     }else if(input == 5){
-        System.out.println("FIVE");
-     }else if(input == 6){
-        System.out.println("SIX");
-     }else if(input == 7){
-        System.out.println("SEVEN");
-     }else if(input == 8){
-        System.out.println("EIGHT");
-     }else if(input == 9){
-        System.out.println("NINE");
-     }else {
-        System.out.println("OTHER");
-     }
-     
+for(int i=1; i<7; i++){
+    for(int o = 1; o<7; o++){
+        int tot = i + o;
+        if (tot == 6){
+            System.out.print( "(" +i +"," +o +")" +" , ");
+        }
+    }
+
+}
+
     }
 }
