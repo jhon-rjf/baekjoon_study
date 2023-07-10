8327a1e3238aa4cd6a491e221974ad979357156a
YunGeol Jung <61873563+jhon-rjf@users.noreply.github.com>
2023-07-10 22:22:08 +0900

    108page 2.

--- src/20230710/Main.java
+++ src/20230710/Main.java
@@ -1,16 +1,31 @@
+import java.util.Scanner;
 public class Main{
- //1번
+ //2번
     public static void main(String[] args) {
-
-        int i;
-        int tot=0;
-        for (i=1;i<101;i++){
-            if ((i>2) && (i % 3==0) || (i>3) && (i % 4==0)){
-                tot=tot+i;
-                System.out.println("i="+i);
-                System.out.println("tot="+tot);
-            }
-        }
-        System.out.println(tot);
+        Scanner sc = new Scanner(System.in);
+       System.out.print("정수를 입력하세요:");
+        int input= sc.nextInt();
+     if(input == 1){
+        System.out.println("ONE");
+     }else if(input == 2){
+        System.out.println("TWO");
+     }else if(input == 3){
+        System.out.println("THREE");
+     }else if(input == 4){
+        System.out.println("FOUR");
+     }else if(input == 5){
+        System.out.println("FIVE");
+     }else if(input == 6){
+        System.out.println("SIX");
+     }else if(input == 7){
+        System.out.println("SEVEN");
+     }else if(input == 8){
+        System.out.println("EIGHT");
+     }else if(input == 9){
+        System.out.println("NINE");
+     }else {
+        System.out.println("OTHER");
+     }
+     
     }
 }
