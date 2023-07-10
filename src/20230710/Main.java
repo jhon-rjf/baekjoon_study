c48b97bd5bd412f336eaaa131d25193d67c1ce30
YunGeol Jung <61873563+jhon-rjf@users.noreply.github.com>
2023-07-10 22:15:29 +0900

    programing excercise 108p

---
+++ src/20230710/Main.java
@@ -0,0 +1,15 @@
+public class Main{
+    public static void main(String[] args) {
+
+        int i;
+        int tot=0;
+        for (i=1;i<101;i++){
+            if ((i>2) && (i % 3==0) || (i>3) && (i % 4==0)){
+                tot=tot+i;
+                System.out.println("i="+i);
+                System.out.println("tot="+tot);
+            }
+        }
+        System.out.println(tot);
+    }
+}
